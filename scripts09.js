b56972351cd494cc47d1080afa093c75e2aefc6d
Daniel Landi Macário <landi_daniel@hotmail.com>
2022-02-14 10:19:37 -0300

    Add files via upload

---
+++ scripts09.js
@@ -0,0 +1,11 @@
+// Criar uma função que exibe a quantidade de *
+// que aquela linha possui
+exibirAsteriscos(05)
+
+function exibirAsteriscos(linhas) {
+    let padrao = '';
+    for(let linha = 1; linha <= linhas; linha ++){
+        padrao += '*';
+        console.log(padrao);
+    }
+}
